2050235be406f5f90d2d5fe6f0a7cb82e318d62f
Siddhant Medar <sidmedar21@gmail.com>
2026-04-30 23:36:31 -0500

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,71 @@
+class ListNode:
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
+        self.nxt = None
+
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 3313
+        self.bucket = []
+
+        for i in range(self.size):
+            self.bucket.append(ListNode(-1,-1))
+
+    def _insert(self,idx,key,val):
+        head = self.bucket[idx]
+        
+        node = self._search(idx,key)[0]
+
+        if node:
+            node.val = val
+            return
+        
+        node = ListNode(key,val)
+        
+        ref = head.nxt
+        head.nxt = node
+        node.nxt = ref
+    
+    def put(self, key: int, value: int) -> None:
+        idx = key%self.size
+        self._insert(idx,key,value)
+
+    def _search(self,idx,key):
+        head = self.bucket[idx]
+        
+        while head:
+            if head.key==key:
+                return head, head.val
+            head = head.nxt
+
+        return None,-1
+
+    def get(self, key: int) -> int:
+        idx = key%self.size
+        return self._search(idx,key)[1]
+        
+    def _remove(self,idx,key):
+        pre = self.bucket[idx]
+        curr = pre.nxt
+
+        while curr:
+            if curr.key==key:
+                break
+            pre = curr
+            curr = curr.nxt
+
+        if curr:
+            pre.nxt = curr.nxt
+    
+    def remove(self, key: int) -> None:
+        idx = key%self.size
+        self._remove(idx,key)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)class ListNode:
